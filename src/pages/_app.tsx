86ce2d8c078eccf14f058fe98bc7e6ce992074d5
Nainoor Patel <nainoor_patel@emertxe.com>
2022-05-12 13:44:00 +0530

    Changed the Nav menu of the home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,9 +18,6 @@ import { NotificationTemplate } from "@components/atoms";
 import { ServiceWorkerProvider } from "@components/containers";
 import { defaultTheme, GlobalStyle } from "@styles";
 import 'bootstrap/dist/css/bootstrap.css';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { NextQueryParamProvider } from "@temp/components";
 import { getSaleorApi, getShopConfig, ShopConfig } from "@utils/ssr";
 
