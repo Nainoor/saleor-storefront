f33a7104df2d6e08d5869d338bb491fab349bea0
Nainoor Patel <nainoor_patel@emertxe.com>
2022-06-03 19:34:00 +0530

    Added Facebook Pixel Code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -89,7 +89,27 @@ const App = ({
       <link rel="manifest" href="/manifest.json" />  
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz@6..96&family=Inter&display=swap" rel="stylesheet"/>      
+      <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz@6..96&family=Inter&display=swap" rel="stylesheet"/>     
+      {/* <!-- Meta Pixel Code --> */}
+     
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '324771406342674');
+          `,
+        }}
+      />
+      {/* <!-- End Meta Pixel Code -->  */}
     </Head>
     <ThemeProvider theme={defaultTheme}>
       <AlertProvider
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        {/* <Head /> */}
+        <Head>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=324771406342674&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+        </Head>
         <body>
           <Main />
           <NextScript />
